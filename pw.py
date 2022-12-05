9879897891cc47d3cbf21cb69ed504f8e4260118
jiahaom <32929553+jiahaom@users.noreply.github.com>
2022-12-05 15:53:36 +0000

    Password_Generator

---
+++ pw.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import win32com.client
+import hashlib
+import datetime
+
+
+st.write("""# Password Manager""")
+with st.expander("To-Do-List"):
+    st.markdown('''
+    ✅Password Generator;
+
+    📌Excel Encryption;
+
+    ''')
+st.write("""## Password Generator""")
+col1, col2, col3 = st.columns(3)
+with col1:
+    pw_case = st.text_input('SF Case Number','0')
+    pw_case = int(float(pw_case))
+with col2:
+    pw_date = st.date_input(
+        "Report Create Date",
+        datetime.date(2022, 8, 1)).strftime("%Y%m%d")
+    pw_date = int(pw_date)
+with col3:
+    pw_author= st.text_input('Author Name', 'Guido van Rossum')
+    pw_author= sum(list(pw_author.encode('utf-8')))
+
+pw = st.button('Generate Password')
+if pw:
+    # temp_pw = hashlib.sha3_256(b'dsadas').hexdigest()
+    temp_pw = str(hex(pw_case+pw_date+pw_author))
+    temp_pw = hashlib.sha3_256(temp_pw.encode('utf-8')).hexdigest()
+    temp_pw = temp_pw[0:4] +'_'+ temp_pw[4:8]+'_'+ temp_pw[8:12]
+    temp_pw = 'Your revertible Password is: \n' + str (temp_pw)
+    st.info(temp_pw, icon = "ℹ️")
+
+st.write(""" ---""")
+st.write("""## Excel Encryption""")
