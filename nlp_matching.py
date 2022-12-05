da08a5cbde47f3818d29c77e08fde434d8a48d5f
jiahaom <32929553+jiahaom@users.noreply.github.com>
2022-12-05 16:30:41 +0000

    Update nlp_matching.py

--- nlp_matching.py
+++ nlp_matching.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
-import SentenceTransformer
+# import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import time
 import numpy as np
